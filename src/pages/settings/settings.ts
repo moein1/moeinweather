3f727a0ffb25c78df4e4e181cc0eb627d6ca02d0
mohammad karimi <m35_karimi@yahoo.com>
2018-02-05 18:26:09 +1000

    Add setting page for changing the default location

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the SettingsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import {Storage} from '@ionic/storage';
+import {HomePage} from '../home/home';
 
 @IonicPage()
 @Component({
@@ -14,12 +9,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'settings.html',
 })
 export class SettingsPage {
+  city : string;
+  state : string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+     public navParams: NavParams,
+    private storage : Storage) {
+       this.storage.get('location').
+       then(location=>{
+        if(location){
+          this.city = location.city;
+          this.state = location.state;
+        }else{
+          this.city = 'Miami';
+          this.state ='FL';
+          this.storage.set('location',{state : 'FL' , city : 'Miami'});
+        }
+       })      
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SettingsPage');
+  saveForm = ()=>{
+    this.storage.set('location' , {state : this.state , city : this.city});
+    this.navCtrl.push(HomePage);
+
   }
 
 }
