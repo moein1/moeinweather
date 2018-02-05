6034e7233032b10dd5b298ff1c3a577b896085e9
mohammad karimi <m35_karimi@yahoo.com>
2018-02-05 16:43:11 +1000

    Get the current temperature data  from the api and show properly in the home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {WeatherProvider} from '../../providers/weather/weather';
 
-@Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
-})
+@Component({selector: 'page-home', templateUrl: 'home.html'})
 export class HomePage {
+  weather : any;
+  location : {
+    state: string,
+    city: string
+  }
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl : NavController, private weatherProvider : WeatherProvider) {}
 
+  ionViewWillEnter = () => {
+    this.location = {
+      state: 'FL',
+      city: 'Miami'
+    }
+    this
+      .weatherProvider
+      .getWeather(this.location.city, this.location.state)
+      .subscribe(response => {
+        console.log(response);
+        this.weather = response.current_observation; 
+      })
   }
 
 }
